5f2d759804117ad21a56cfa03b277aada4486268
RogersiCamejo <85595626+RogersiCamejo@users.noreply.github.com>
2021-07-15 10:00:21 -0400

    Add files via upload

---
+++ mi primer programa PHP.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Primer Programa en PHP</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+echo "Hola, mi nombre es Rogersi Camejo.
+Soy de Venezuela.
+Tengo 28 años.
+Mi hobbie es leer, viajar, cantar, bailar y tocar el violin.
+Mi editor favorito es Visual Studio Code.
+El sistema operativo que uso es macOS Big Sur"
+
+?>
+
+</body>
+</html>
